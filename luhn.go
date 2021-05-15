5bdac2f458b84d27676c7060d1d0b37d594b782e
shanep <shanecandoit@gmail.com>
2021-05-15 10:58:40 -0500

    clean up

--- luhn.go
+++ luhn.go
@@ -7,47 +7,35 @@ import (
 
 // Valid is a card number valid?
 func Valid(card string) bool {
-	// fmt.Print("Valid? ", card, "\t")
 
-	sum := 0
-
-	// remove spaces
 	card = strings.ReplaceAll(card, " ", "")
 
-	// count down instead of up
 	count := len(card)
 	if count < 2 {
 		return false
 	}
 
+	sum := 0
 	for _, ch := range card {
 
-		// fail on non digit
 		if !unicode.IsDigit(ch) {
 			return false
 		}
 
-		// int('0') - int('0') = 0, '1' - '0' = 1
 		n := int(ch) - int('0')
-
 		// double every second digit
-		// starting from the right
 		isSecond := count%2 == 0
 		if isSecond {
 
-			// If doubling the number results greater than 9
-			// then subtract 9 from the product
 			prod := 2 * n
 			if prod > 9 {
 				prod -= 9
 			}
-			//sum += prod
 			n = prod
 		}
 		sum += n
 		count--
 	}
 
-	result := sum%10 == 0
-	return result
+	return sum%10 == 0
 }
